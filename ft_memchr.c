4a0ec757b0b4fbca1676df8f504bf871c982dc75
Loic Humbert <loichumbert5450@gmail.com>
2021-10-20 19:20:41 +0200

    Fixed as Tripouille tests !

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,26 +6,25 @@
 /*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 17:56:59 by lhumbert          #+#    #+#             */
-/*   Updated: 2021/10/19 01:47:17 by lhumbert         ###   ########.fr       */
+/*   Updated: 2021/10/20 19:12:19 by lhumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char	*str;
-	unsigned char	b;
+	const char	*str;
+	size_t		i;
 
-	str = (unsigned char*)s;
-	b = (unsigned char)c;
-	while (*str && n--)
+	str = (const char*)s;
+	i = 0;
+	while (i < n)
 	{
-		if (*str == b)
-			return (str);
-		str++;
+		if (str[i] == (char)c)
+			return ((void*)&str[i]);
+		i++;
 	}
-	if (*str == '\0' && b == '\0')
-		return (str);
 	return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 03:02:12 by lhumbert          #+#    #+#             */
-/*   Updated: 2021/10/19 03:10:54 by lhumbert         ###   ########.fr       */
+/*   Updated: 2021/10/20 19:18:22 by lhumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!*s1)
+	if (n && !*s1)
 		return (0 - *s2);
 	while (n-- && *s1)
 	{
 		if (*(s1++) != *(s2++))
 			return ((unsigned char)*(--s1) - (unsigned char)*(--s2));
+		if (!*s1 && *s2)
+			return (0 - *s2);
 	}
 	return (0);
 }
