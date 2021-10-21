6d121b54e39bc7a940ee6e22b54f0658b6966d75
Loic Humbert <loichumbert5450@gmail.com>
2021-10-22 01:50:52 +0200

    strncmp ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 03:02:12 by lhumbert          #+#    #+#             */
-/*   Updated: 2021/10/20 19:18:22 by lhumbert         ###   ########.fr       */
+/*   Updated: 2021/10/22 01:46:04 by lhumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (n && !*s1)
 		return (0 - *s2);
-	while (n-- && *s1)
+	while (n--)
 	{
-		if (*(s1++) != *(s2++))
-			return ((unsigned char)*(--s1) - (unsigned char)*(--s2));
+		if (*s1 != *s2)
+			return ((unsigned char)*(s1) - (unsigned char)*(s2));
 		if (!*s1 && *s2)
-			return (0 - *s2);
+			return ((unsigned char)(0 - *s2));
+		if (*s1)
+			s1++;
+		if (*s2)
+			s2++;
 	}
 	return (0);
 }
