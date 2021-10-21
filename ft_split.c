09ce4241f59eba8e84cd99f41290709872d2ad3b
Loic Humbert <loichumbert5450@gmail.com>
2021-10-21 20:15:05 +0200

    YES

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 19:41:24 by lhumbert          #+#    #+#             */
-/*   Updated: 2021/10/21 20:14:00 by lhumbert         ###   ########.fr       */
+/*   Updated: 2021/10/21 20:14:54 by lhumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,26 +39,21 @@ char	**ft_split(char const *s, char c)
 	if (!s)
 		return (NULL);
 	asize = ft_cntparts(s, c);
-	printf("nb parts: %zu\n", asize);
 	ret = (char **)malloc(sizeof(char *) * (asize + 1));
 	if (!ret)
 		return (NULL);
 	while (*s)
 	{
-		printf("s: %s\n", s);
 		while (*s == c)
 			s++;
 		len = 0;
 		while (s[len] && s[len] != c)
 			len++;
-		printf("slen: %zu\n", len);
-		printf("first: %c, last: %c\n", *s, s[len - 1]);
 		if (len)
 			*(ret++) = ft_substr(s, 0, len);
 		s += len;
 	}
 	*ret = NULL;
 	ret -= asize;
-	printf("ret[0] = \"%s\"", ret[0]);
 	return (ret);
 }
