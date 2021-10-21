c249fb8e60ee45816df0515d579f6b88414c4db8
Loic Humbert <loichumbert5450@gmail.com>
2021-10-21 02:23:38 +0200

    join and trim done

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 00:49:54 by lhumbert          #+#    #+#             */
+/*   Updated: 2021/10/21 00:57:51 by lhumbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	l1;
+	size_t	l2;
+	char	*ret;
+
+	l1 = ft_strlen((char *)s1);
+	l2 = ft_strlen((char *)s2);
+	ret = (char *)malloc((l1 + l2 + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+	while (*s1)
+		*(ret++) = *(s1++);
+	while (*s2)
+		*(ret++) = *(s2++);
+	*ret = '\0';
+	return (ret - l1 - l2);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 01:00:16 by lhumbert          #+#    #+#             */
+/*   Updated: 2021/10/21 02:21:56 by lhumbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char	ft_inset(char c, char const *set)
+{
+	if (!c)
+		return (0);
+	while (*set)
+	{
+		if (*(set++) == c)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*ret;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	while (ft_inset(*s1, set))
+	{
+		s1++;
+		len--;
+	}
+	while (len && ft_inset(s1[len - 1], set))
+		len--;
+	ret = (char *)malloc((len + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+	ret[len] = '\0';
+	if (!len)
+		return (ret);
+	while (len--)
+		ret[len] = s1[len];
+	return (ret);
+}
