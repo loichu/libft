c194178a056549bfe62bc5a96b4d55099e0dd5e1
Loic Humbert <loichumbert5450@gmail.com>
2021-10-22 00:39:50 +0200

    norminette

--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,23 @@
 /*   By: lhumbert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 17:26:17 by lhumbert          #+#    #+#             */
-/*   Updated: 2021/10/20 17:55:41 by lhumbert         ###   ########.fr       */
+/*   Updated: 2021/10/22 00:39:31 by lhumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+
+char	*ft_empty(void)
+{
+	char	*ret;
+
+	ret = (char *)malloc(1 * sizeof(char));
+	if (!ret)
+		return (0);
+	*ret = '\0';
+	return (ret);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -20,25 +31,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ret;
 	size_t	i;
 
-	slen = ft_strlen((char*)s);
+	if (!s)
+		return (NULL);
+	slen = ft_strlen((char *)s);
 	if (start >= slen)
-	{
-		ret = (char*)malloc(1 * sizeof(char));
-		if (!ret)
-			return (0);
-		*ret = '\0';
-		return (ret);
-	}
+		return (ft_empty());
+	rlen = len;
 	if (start + len > slen)
 		rlen = (slen - start + 1);
-	else
-		rlen = len;
-	ret = (char*)malloc((rlen + 1) * sizeof(char));
+	ret = (char *)malloc((rlen + 1) * sizeof(char));
 	if (!ret)
 		return (0);
-	//s += start;
 	i = -1;
-	//ret[rlen] = '\0';
 	while (++i < rlen)
 		ret[i] = s[start + i];
 	ret[i] = '\0';
